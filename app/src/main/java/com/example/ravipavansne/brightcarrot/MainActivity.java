6f36254c4fb08936e07e19444306ea83663fcc04
sne <snehishdasari@gmail.com>
2018-12-18 19:18:27 +0530

    Signup Half done

--- app/src/main/java/com/example/ravipavansne/brightcarrot/MainActivity.java
+++ app/src/main/java/com/example/ravipavansne/brightcarrot/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ravipavansne.brightcarrot;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager ;
     private LinearLayout linearLayout ;
     private slideradapter slideradapter1 ;
+    private Button signbutton ;
+    private Button loginbutton ;
 
 
     @Override
@@ -23,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
         slideradapter1 = new slideradapter(this) ;
         viewPager.setAdapter(slideradapter1) ;
 
+        signbutton = (Button) findViewById(R.id.signbutton) ;
+        loginbutton = (Button) findViewById(R.id.loginbutton) ;
+        signbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignupActivity.class));
+            }
+        });
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/ravipavansne/brightcarrot/SignupActivity.java
@@ -0,0 +1,73 @@
+package com.example.ravipavansne.brightcarrot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    private Spinner day ;
+    private Spinner month ;
+    private Spinner year ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+        day=(Spinner) findViewById(R.id.spinnerday) ;
+        month = (Spinner) findViewById(R.id.spinnermonth) ;
+        year = (Spinner) findViewById(R.id.spinneryear) ;
+
+        ArrayList<String> days = new ArrayList<>() ;
+        for(int i=1;i<32;i++)
+        {
+            String k = String.valueOf(i) ;
+            days.add(k) ;
+        }
+
+        ArrayList<String> months = new ArrayList<>() ;
+        months.add("January") ;
+        months.add("February") ;
+        months.add("March") ;
+        months.add("April") ;
+        months.add("May") ;
+        months.add("June") ;
+        months.add("July") ;
+        months.add("August") ;
+        months.add("September") ;months.add("October") ;
+        months.add("November") ;
+        months.add("December") ;
+
+        ArrayList<String> years = new ArrayList<>() ;
+        for(int i=2000;i>1949;i--)
+        {
+            String k = String.valueOf(i) ;
+            years.add(k) ;
+        }
+
+
+        ArrayAdapter<String> dayadapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,days) ;
+        ArrayAdapter<String> monthadapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,months) ;
+        ArrayAdapter<String> yearadapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,years) ;
+
+
+        day.setAdapter(dayadapter);
+        month.setAdapter(monthadapter);
+        year.setAdapter(yearadapter) ;
+
+
+
+
+
+
+
+
+
+    }
+}
