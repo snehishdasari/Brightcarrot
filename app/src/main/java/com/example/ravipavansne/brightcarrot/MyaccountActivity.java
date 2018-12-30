609926a2896baf965d464784cd9f8bd8c020fe77
ravitej <ravitejmaheshwaram@gmail.com>
2018-12-29 23:23:02 -0600

    after nightout

--- app/src/main/java/com/example/ravipavansne/brightcarrot/MyaccountActivity.java
+++ app/src/main/java/com/example/ravipavansne/brightcarrot/MyaccountActivity.java
@@ -1,17 +1,31 @@
 package com.example.ravipavansne.brightcarrot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MyaccountActivity extends AppCompatActivity {
 
     private Toolbar toolbar ;
+    private CircleImageView backaarrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myaccount);
 
+    backaarrow = (CircleImageView) findViewById(R.id.backmyaccount);
+    backaarrow.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MyaccountActivity.this,Home2Activity.class));
+        }
+    });
 
     }
 }
