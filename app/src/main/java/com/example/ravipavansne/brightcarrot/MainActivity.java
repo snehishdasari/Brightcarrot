958e1a46ff5cfe25be978e6addcf1eadf7c241b5
sne <snehishdasari@gmail.com>
2019-01-18 21:32:37 +0530

    Changed starting page

--- app/src/main/java/com/example/ravipavansne/brightcarrot/MainActivity.java
+++ app/src/main/java/com/example/ravipavansne/brightcarrot/MainActivity.java
@@ -5,14 +5,20 @@ import android.content.Intent;
 import android.content.pm.Signature ;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -27,10 +33,12 @@ import java.security.NoSuchAlgorithmException;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
-    private LinearLayout linearLayout;
+    private LinearLayout dotlayout;
     private slideradapter slideradapter1;
-    private Button signbutton;
-    private Button loginbutton;
+    private Button skipbutton;
+    private TextView []dots ;
+    private int []layouts ;
+    private Button nextbutton;
     private FirebaseUser fuser;
 
 
@@ -39,14 +47,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        viewPager = (ViewPager) findViewById(R.id.slide1);
-        linearLayout = (LinearLayout) findViewById(R.id.sign);
-        slideradapter1 = new slideradapter(this);
-        viewPager.setAdapter(slideradapter1);
         fuser = FirebaseAuth.getInstance().getCurrentUser();
         if (fuser != null) {
-             {
+            {
                 Intent i = new Intent(MainActivity.this, Home2Activity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(i);
@@ -54,43 +57,95 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-        printhashkey();
-        signbutton = (Button) findViewById(R.id.signbutton);
-        loginbutton = (Button) findViewById(R.id.loginbutton);
-        signbutton.setOnClickListener(new View.OnClickListener() {
+
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        dotlayout = (LinearLayout) findViewById(R.id.dotLayout);
+        layouts = new int[]{R.layout.slider_cars,R.layout.slider_bikes,R.layout.slider_hv} ;
+        slideradapter1 = new slideradapter(layouts,getApplicationContext());
+        viewPager.setAdapter(slideradapter1);
+
+       // printhashkey();
+        skipbutton = (Button) findViewById(R.id.skipbutton);
+        nextbutton = (Button) findViewById(R.id.nextbutton);
+
+
+        setStatusTransparent();
+        skipbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, signup2Activity.class));
             }
         });
 
-        loginbutton.setOnClickListener(new View.OnClickListener() {
+        nextbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+        setDotStatus(0);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if(i==layouts.length-1)
+                {
+                    nextbutton.setText("start");
+                    skipbutton.setVisibility(View.GONE);
+                }
+                else
+                {
+                    nextbutton.setText("next");
+                    skipbutton.setVisibility(View.VISIBLE);
+                }
+                setDotStatus(i);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
 
 
-    public void printhashkey() {
+    private void setDotStatus(int page)
+    {
+        dotlayout.removeAllViews();
+        dots  = new TextView[layouts.length] ;
+        for(int i=0;i<dots.length;i++)
+        {
+            dots[i]= new TextView(this) ;
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(30) ;
+            dots[i].setTextColor(Color.parseColor("#a9b4bb"));
+            dotlayout.addView(dots[i]);
+        }
+        if(dots.length>0)
+        {
+            dots[page].setTextColor(Color.parseColor("#ffffff"));
+        }
+    }
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.mytrendin.keyhash",
-                    PackageManager.GET_SIGNATURES);
-            for (android.content.pm.Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
 
-        } catch (NoSuchAlgorithmException e) {
+
+
+    private void setStatusTransparent()
+    {
+        if(Build.VERSION.SDK_INT >=21)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_FULLSCREEN);
+            Window window = getWindow() ;
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS) ;
+            window.setStatusBarColor(Color.TRANSPARENT);
 
         }
-
-
     }
 
 }
--- app/src/main/java/com/example/ravipavansne/brightcarrot/slideradapter.java
+++ app/src/main/java/com/example/ravipavansne/brightcarrot/slideradapter.java
@@ -15,38 +15,26 @@ public class slideradapter extends PagerAdapter {
 
     Context context ;
     LayoutInflater layoutInflater ;
+    private int[] layouts ;
     /**
      * Return the number of views available.
      */
 
-    public slideradapter (Context context)
+    public slideradapter (int layouts[],Context context)
     {
+        this.layouts = layouts ;
         this.context = context ;
 
     }
 
-    public int slide_images[]= {
-            R.drawable.carimage,
-            R.drawable.bikeimage,
-            R.drawable.hvimage
-    } ;
-    public String slide_headings[]=
-            {
-                    "CARS","BIKES","HEAVY VEHICLES"
-            };
-    public String slide_descs[]={
-            "For Small trips with family and friends",
-            "For  emergencies and Work purposes",
-            "For Transferring from one place to other place"
-    } ;
     @Override
     public int getCount() {
-        return slide_headings.length;
+        return layouts.length;
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-        return view == (RelativeLayout) o;
+        return view ==  o;
     }
 
 
@@ -54,15 +42,9 @@ public class slideradapter extends PagerAdapter {
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         layoutInflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE) ;
-        View view = layoutInflater.inflate(R.layout.slide_layout,container,false) ;
+        View view = layoutInflater.inflate(layouts[position],container,false) ;
 
-        CircleImageView slideimageview = (CircleImageView) view.findViewById(R.id.slideimage) ;
-        TextView textview = (TextView) view.findViewById(R.id.textView) ;
-        TextView textView2 = (TextView) view.findViewById(R.id.textView2) ;
 
-        slideimageview.setImageResource(slide_images[position]);
-        textview.setText(slide_headings[position]);
-        textView2.setText(slide_descs[position]);
 
 
         container.addView(view);
@@ -71,6 +53,7 @@ public class slideradapter extends PagerAdapter {
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView((RelativeLayout)object);
+        View v = (View) object ;
+        container.removeView(v);
     }
 }
